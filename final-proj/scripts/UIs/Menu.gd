7ee0cb4f29503c572d4c61caab2da0d99ca1825e
Acceltra65535 <work@comm.moe>
2025-12-06 13:43:03 -0800

    Add create server func

--- final-proj/scripts/UIs/Menu.gd
+++ final-proj/scripts/UIs/Menu.gd
@@ -5,6 +5,8 @@ extends Control
 @onready var exit_button: Button = $Menu/VBoxContainer/Exit
 @onready var bgm_player: AudioStreamPlayer = $BgmPlayer
 
+var peer = ENetMultiplayerPeer.new()
+
 func _ready() -> void:
 	bgm_player.play()
 
@@ -28,6 +30,25 @@ func _on_load_pressed() -> void:
 	
 	# Go directly to the game scene (skip cutscene/character select)
 	get_tree().change_scene_to_file("res://scenes/CityOutskirtsIntro.tscn")
+
+func _on_server_button_down() -> void:
+	#Listening port and create server
+	var error = peer.create_server(10000)
+	if error != OK:
+		printerr("Create server failure Code= ", error)
+		return
+	else:
+		print("Create server successfully")
+	multiplayer.multiplayer_peer = peer
+	
+	# Listening another player
+	multiplayer.peer_connected.connect(_on_peer_connected)
+
+func _on_peer_connected(id: int) -> void:
+	pass
+
+func _on_join_button_down() -> void:
+	pass # Replace with function body.
 
 func _on_exit_pressed() -> void:
 	get_tree().quit()
